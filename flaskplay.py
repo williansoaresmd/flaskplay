623538e0c06ba5d24bf36eff5eca7646f6e91652
Willian Soares <williansoaresmd@gmail.com>
2026-01-09 13:34:42 -0300

    criacao da pasta template para orgainzar melhor arquivos html e criancao de uma classe no arquivo principal

--- flaskplay.py
+++ flaskplay.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+class Game:
+    def __init__(self, name, category, console):
+        self.name = name
+        self.category = category
+        self.console = console
+
+
 @app.route("/")
 def hello():
-    return "<h1>Hello, World!!!!</h1>"
+    game1 = Game('GTA', 'action', 'xbox')
+    game2 = Game('Call of Duty', 'war', 'ps5')
+    game3 = Game('Fifa', 'soccer', 'ps5')
+
+    list = [game1, game2, game3]
+    return render_template('lista.html', titulo='Games', jogos=list)
 
 
 app.run()
